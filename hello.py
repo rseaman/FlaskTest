4780bde095f9767022f77ecbce555ca917d3df36
Ubuntu <ubuntu@ip-10-185-172-218.ec2.internal>
2013-11-07 06:51:37 +0000

    cookie test

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import make_response
 
 app = Flask(__name__)
 
@@ -6,6 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/cookietest/')
+def cookie():
+    resp = make_response(render_template('index.html'))
+    resp.set_cookie('username','Riley')
+    return resp
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
